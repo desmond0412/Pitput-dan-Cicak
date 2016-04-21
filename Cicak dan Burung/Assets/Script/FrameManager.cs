9bd73f563f9dbaa3c0ac5c16fd5b0309318fef7c
Desmond <Desmond>
2016-04-22 04:18:13 +0700

    - Added Standard Assets - Added ITween - Added FrameManager

---
+++ Cicak dan Burung/Assets/Script/FrameManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FrameManager : MonoBehaviour {
+	[SerializeField]
+	static AbstractFrame currentFrame;
+
+	public List<AbstractFrame> frameList;
+	int currentPage = 0;
+
+	void Awake(){
+		if (frameList.Count <= 0) {
+			Debug.LogWarning ("No Frame Detected");
+		}
+	}
+
+	void Start(){
+		currentPage = 0;
+		SetupFrame ();
+	}
+
+	public void SetupFrame(){
+		currentFrame = frameList [currentPage];
+
+		foreach (AbstractFrame frame in frameList) {
+			if (frame.Equals (currentFrame)) {
+				frame.gameObject.SetActive (true);
+			} else {
+				frame.gameObject.SetActive (false);
+			}
+		}
+	}
+
+	public void NextPage(){
+		if (currentPage >= frameList.Count - 1) {
+			return;
+		}
+		currentPage++;
+
+		SetupFrame ();
+	}
+
+	public void PrevPage(){
+		if (currentPage <= 0) {
+			return;
+		}
+		currentPage--;
+
+		SetupFrame ();
+	}
+}
