6fbcb3097803b5e5e1d31283947208ccf7398fe1
Desmond <Desmond>
2016-04-22 04:19:28 +0700

    - Added Assets for Frame 1 - Added Script for Frame Controller - Added Main Menu

---
+++ Cicak dan Burung/Assets/Script/AbstractFrame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbstractFrame : MonoBehaviour {
+	bool isFinish;
+
+	void Awake(){
+		isFinish = false;
+	}
+
+
+}
---
+++ Cicak dan Burung/Assets/Script/Frame Controller/Frame6Controller.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Frame6Controller : AbstractFrame {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
